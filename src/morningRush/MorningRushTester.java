a1008d46713e818a0e14121a852bca444ae08286
Jason Myers <Jason.myers-1@ou.edu>
2018-11-07 14:30:46 -0600

    Working program

--- src/morningRush/MorningRushTester.java
+++ src/morningRush/MorningRushTester.java
@@ -49,7 +49,7 @@ public class MorningRushTester
                         gulps = initialGulps;
                         
                     }
-                    gulps--;
+                    --gulps;
                 }
             }
             
